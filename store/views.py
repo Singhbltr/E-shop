dac915fea108c7802a094d7f420f5e855be7b649
Singbltr <saurabhsingh1625@gmail.com>
2021-01-11 19:48:37 +0530

    init

--- store/views.py
+++ store/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models.products import Product
+from .models.category import Category
 # Create your views here.
 def index(request):
     products=Product.get_all_products();
-    print(products)
-    return render(request,'index.html',{'products':products})
+    categories=Category.get_all_categories()
+    data={}
+    data['products']=products
+    data['categories']=categories
+    #return render(request,'orders/order.html')
+    #print(products)
+
+    return render(request,'index.html',data)
